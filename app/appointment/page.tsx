f4d7058e10de017a98ade6f437dad7d7ae6a9d95
Anurag Dash <anuragdash004@gmail.com>
2026-04-07 01:03:42 +0530

    reduce pattern density

--- app/appointment/page.tsx
+++ app/appointment/page.tsx
@@ -36,7 +36,7 @@ export default function AppointmentPage() {
 
         // 🔥 BIGGER PATTERN (CLEARLY VISIBLE NOW)
         backgroundImage: "url('/pattern.png')",
-        backgroundSize: "90px", // 👈 STRONG zoom (change to 80 if you want even bigger)
+        backgroundSize: "180px", // 👈 STRONG zoom (change to 80 if you want even bigger)
         backgroundRepeat: "repeat",
       }}
     >
